685378e67c04b70e1d49054bf7bae98df86f8a41
Mac McDell <macmcdell@gmail.com>
2012-04-12 11:57:03 -0700

    initial push

---
+++ Trunk/BankTestProjects/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankTestProjects
+{
+    public class BankAccount
+    {
+        public float Balance
+        { get; set; }
+
+        public float MinimumBalance
+        { get; set; }
+
+        public void Deposit(float Amount)
+        {
+            Balance += Amount;
+        }
+
+        public void Withdraw(float Amount)
+        {
+            Balance -= Amount;
+        }
+
+        public void TransferFunds(BankAccount destination, float amount)
+        {
+            destination.Deposit(amount);
+            if (Balance - amount < MinimumBalance)
+                throw new InsufficientFundsException();
+
+            Withdraw(amount);
+        }
+
+
+
+    }
+
+    public class InsufficientFundsException : ApplicationException
+    {
+    
+    
+    }
+}
---
+++ Trunk/Nunit1/BankAccountTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+
+namespace BankTestProjects
+{
+    using NUnit.Framework;
+    [TestFixture]
+
+    public class BankAccountTest
+    {
+        BankAccount source;
+        BankAccount destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new BankAccount();
+            source.Deposit(200.00F);
+            destination = new BankAccount();
+            destination.Deposit(150.00F);
+           
+        }
+
+
+        [Test]
+        public void TransferFunds()
+        {
+
+        //    source.Deposit(200.00F);
+        //    destination.Deposit(150.00F);
+
+            source.TransferFunds(destination, 100.00F);
+            Assert.AreEqual(250.00F, destination.Balance);
+            Assert.AreEqual(100.00F, source.Balance);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InsufficientFundsException))]
+        public void TransferWithInsufficientFunds()
+        {
+        //    BankAccount source = new BankAccount();
+        //    source.Deposit(250.00F);
+       //     BankAccount destination = new BankAccount();
+       //     destination.Deposit(150.00F);
+            source.TransferFunds(destination, 300.00F);
+        }
+
+        [Test]
+        [Ignore("Check the logic of the transfer funds method")]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+        //    BankAccount source = new BankAccount();
+        //    source.Deposit(200.00F);
+        //    BankAccount destination = new BankAccount();
+        //    destination.Deposit(150.00F);
+            try
+            {
+                source.TransferFunds(destination, 300.00F);
+            }
+            catch (InsufficientFundsException expected)
+            {
+            }
+
+            Assert.AreEqual(200.00F, source.Balance);
+            Assert.AreEqual(150.00F, destination.Balance);
+
+        }
+    }
+}
