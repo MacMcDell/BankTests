9355c5d333af4ed3105e5e7cfceaa63c11cadbdf
Mac McDell <macmcdell@gmail.com>
2012-04-13 12:58:44 -0700

    big push

--- Trunk/BankTestProjects/BankAccount.cs
+++ Trunk/BankTestProjects/BankAccount.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Web;
 
 namespace BankTestProjects
-{
-    public class BankAccount
     {
+    public class BankAccount
+        {
         public float Balance
         { get; set; }
 
@@ -14,35 +14,34 @@ namespace BankTestProjects
         { get; set; }
 
         public void Deposit(float Amount)
-        {
+            {
             Balance += Amount;
-        }
+            }
 
         public void Withdraw(float Amount)
-        {
+            {
             Balance -= Amount;
-        }
+            }
 
-        public int TransferFunds(BankAccount destination, float amount)
-        {
-            destination.Deposit(amount);
-            if (Balance - amount < MinimumBalance)
-                {
-               
-                return 0; 
-                throw new InsufficientFundsException();
-                }
-
+        public int TransferFunds(BankAccount destination, float amount, BankAccount source)
+            {
+            if (source.Balance < amount) return 0; // throw new InsufficientFundsException(); 
+ if (source.Balance - amount < MinimumBalance) return 0; // throw new InsufficientFundsException();
+           
+           
+ destination.Deposit(amount);
             Withdraw(amount);
             return 1;
+
+            }
+
+
+
         }
 
-
-
-    }
-
     public class InsufficientFundsException : ApplicationException
-    {
-        
+        {
+        public int InF()
+        { return 0; }
+        }
     }
-}
